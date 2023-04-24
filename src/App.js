4201d644ba534dcb5db08add093efec69dc4424f
yeonupark <idepix@naver.com>
2023-04-24 19:45:01 +0900

    주소입력및변환기능추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from "leaflet";
 import proj4 from 'proj4';
-//import 'proj4leaflet';
+import React, { useState, useEffect, useCallback } from 'react';
+
+/*global kakao*/
 
 const position = [36.0761696194201, 129.382655928438] // 위,경도 직접 입력할때의 값
 
@@ -29,7 +31,7 @@ function App() {
     shadowSize: [41, 41],
     shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
   });
-
+  
   return (
     <MapContainer center={position} zoom={30} scrollWheelZoom={false} style={{ width: "100%", height: "100vh" }}>
       <TileLayer
@@ -41,8 +43,80 @@ function App() {
         </Popup>
       </Marker>
     </MapContainer>
-      
   );
+  
 }
 
-export default App;
+
+function LocationInput() {
+  const [search, setSearch] = useState("");
+  const onChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+
+  var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 3 // 지도의 확대 레벨
+    };
+
+  // 지도를 생성합니다    
+  var map = new kakao.maps.Map(mapContainer, mapOption);
+
+  // 주소-좌표 변환 객체를 생성합니다
+  var geocoder = new kakao.maps.services.Geocoder();
+
+  const onSearch = () => {
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(search, function (result, status) {
+
+      // 정상적으로 검색이 완료됐으면 
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+      }
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: coords
+      });
+
+      map.setCenter(coords)
+
+      // json 생성 (위도, 경도, 반경)
+      const pos = { x: String(coords.La), y: String(coords.Ma), position_range: "100" }
+      const user1 = JSON.stringify(pos)
+      console.log(user1)
+      
+      fetch('/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: user1
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Data received:', data);
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
+    });
+    
+  }
+  return (
+    <>
+      <input type="text" value={search} onChange={onChange}></input>
+      <button onClick={onSearch}>Search</button>
+    </>
+  )
+}
+
+
+export default LocationInput;
