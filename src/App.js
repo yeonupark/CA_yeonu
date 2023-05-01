35437df99bda822c28d8e99df119130558ebf4a4
yeonupark <idepix@naver.com>
2023-05-01 17:19:22 +0900

    Positions 파일 분리, post 수정, get 추가

--- src/App.js
+++ src/App.js
@@ -8,52 +8,11 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 /*global kakao*/
 
-const position = [36.0761696194201, 129.382655928438] // 위,경도 직접 입력할때의 값
-
-const crs2097 = '+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs';
-const crs5174 = '+proj=tmerc +lat_0=38 +lon_0=127.00289 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs';
-const crsWgs84 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
-
-// EPSG 2097 또는 EPSG 5174 좌표계 사용할 때 원본 x,y 좌표 값
-const x = 414570.563877435;
-const y = 288815.472861785;
-
-// x,y 좌표 (EPSG 2097 or EPSG 5174) to 위,경도 (WGS84) 변환
-const [lng, lat] = proj4(crs5174, crsWgs84, [x, y]);
-
 function App() {
-
-  const customMarker = L.icon({
-    iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  });
-  
-  return (
-    <MapContainer center={position} zoom={30} scrollWheelZoom={false} style={{ width: "100%", height: "100vh" }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position} icon={customMarker}>
-        <Popup>
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
-  
-}
-
-
-function LocationInput() {
   const [search, setSearch] = useState("");
   const onChange = (e) => {
     setSearch(e.target.value)
   }
-
 
   var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
@@ -66,6 +25,9 @@ function LocationInput() {
 
   // 주소-좌표 변환 객체를 생성
   var geocoder = new kakao.maps.services.Geocoder();
+  
+  // 서버에서 전달받은 편의시설 데이터 넣어줄 공간
+  const [place, setPlace] = useState([]);
 
   const onSearch = () => {
     // 주소로 좌표를 검색
@@ -75,12 +37,13 @@ function LocationInput() {
       if (status === kakao.maps.services.Status.OK) {
         var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
       }
+      /*
       // 결과값으로 받은 위치를 마커로 표시
       var marker = new kakao.maps.Marker({
         map: map,
         position: coords
       });
-
+      */
       map.setCenter(coords)
 
       // 결과값으로 받은 위도, 경도 + 반경으로 json생성
@@ -88,28 +51,57 @@ function LocationInput() {
       const user1 = JSON.stringify(pos)
       console.log(user1)
       
-      // 서버로 위도,경도,반경 json값 post
-      fetch('/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: user1
+      // 서버에 데이터 전송
+      axios.post("users", {
+        title: "제목",
+        content: "내용",
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      // 서버에서 데이터 받아오기
+      axios.get("users")
+        .then((response) => {
+          setPlace([...response.data]);
+
+          /*
+          // A) 서버에서 받아온 데이터를 이용하여 마커를 생성하고 지도에 표시
+          response.data.forEach(place => {
+            const latLng = new kakao.maps.LatLng(place.y, place.x);
+            const marker = new kakao.maps.Marker({
+              position: latLng,
+              map: map,
+              title: place.name
+            });
+          });
+
+          // B) 1, 2, 3번째 장소 정보 추출
+          const place1 = response.data[0];
+          const place2 = response.data[1];
+          const place3 = response.data[2];
+          
+          const marker1 = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place1.y좌표, place1.x좌표)
+          });
+          const marker2 = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place2.y좌표, place2.x좌표)
+          });
+          const marker3 = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place3.y좌표, place3.x좌표)
+          });
+          */
         })
-        .then(data => {
-          console.log('Data received:', data);
-        })
-        .catch(error => {
-          console.error('There was a problem with the fetch operation:', error);
+        .catch(function (error) {
+          console.log(error);
         });
-    });
-    
+    });   
   }
   return (
     <>
@@ -120,4 +112,4 @@ function LocationInput() {
 }
 
 
-export default LocationInput;
+export default App;
