f7947ff7f4971e19f110fcf0cdb944f757361308
yeonupark <idepix@naver.com>
2023-06-06 03:00:55 +0900

    커스텀 마커로 변경

--- LeafletSearch.js
+++ LeafletSearch.js
@@ -101,26 +101,60 @@ export const LeafletSearch = ({ setSearch }) => {
 
         // 서버로 POST
         try {
-            const response = await axios.post(globalurl+"facilities/info/", user_json);
+            const response = await axios.post(globalurl + "facilities/info/", user_json);
 
             setLocation(response.data.location);
-            
-            const places = [];
-            // 편의시설 종류
-            const facilities = Object.keys(response.data.location.facility_type);
-            
-            // 모든 편의시설 데이터 추가
-            facilities.forEach(facility => {
-                const facilityData = response.data.location.facility_type[facility].place;
-                places.push(...facilityData);
-            });
 
-            places.forEach((place) => {
-                const new_coords = new L.LatLng(place.lat, place.lon);
-                const marker = L.marker(new_coords);
-                marker.bindPopup(`<b>${place.name}</b>`);
-                markerClusterGroup.addLayer(marker);
-            });
+            // const places = [];
+            // // 편의시설 종류
+            // const facilities = Object.keys(response.data.location.facility_type);
+
+            // // 모든 편의시설 데이터 추가
+            // facilities.forEach(facility => {
+            //     const facilityData = response.data.location.facility_type[facility].place;
+            //     places.push(...facilityData);
+            // });
+
+            // places.forEach((place) => {
+            //     const new_coords = new L.LatLng(place.lat, place.lon);
+            //     const marker = L.marker(new_coords);
+            //     marker.bindPopup(`<b>${place.name}</b>`);
+            //     markerClusterGroup.addLayer(marker);
+            // });
+
+            // 편의시설 별 커스텀 마커로 변경
+            const facilities = response.data.location.facility_type;
+
+            const iconPaths = {
+                cafe: "https://api.geoapify.com/v1/icon/?type=material&color=%238c5a47&size=large&icon=coffee&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                hospital: "https://api.geoapify.com/v1/icon/?type=material&color=%23ec3236&size=large&icon=stethoscope&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                pharmacy: "https://api.geoapify.com/v1/icon/?type=material&color=%2357c779&size=large&icon=capsules&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                laundry: "https://api.geoapify.com/v1/icon/?type=material&color=%23eec122&size=x-large&icon=tshirt&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6",
+                bus: "https://api.geoapify.com/v1/icon/?type=material&color=%23579ac7&size=large&icon=bus&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                convenience: "https://api.geoapify.com/v1/icon/?type=material&color=%238a57c7&size=large&icon=clock&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                hair: "https://api.geoapify.com/v1/icon/?type=material&color=%23f06ce4&icon=cut&size=large&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6",
+                metro: "https://api.geoapify.com/v1/icon/?type=material&color=%234445ef&icon=subway&size=large&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                mart: "https://api.geoapify.com/v1/icon/?type=material&color=%23ed7a1e&icon=store&size=large&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6 ",
+                gym: "https://api.geoapify.com/v1/icon/?type=material&color=%23848080&size=x-large&icon=dumbbell&iconType=awesome&apiKey=5d8a4be3945344c2b7b425443b6ae5c6"
+            };
+
+            for (const facility in facilities) {
+                const facilityData = facilities[facility].place;
+                facilityData.forEach((place) => {
+                    const new_coords = new L.LatLng(place.lat, place.lon);
+
+                    const iconPath = iconPaths[facility];
+                    
+                    const customIcon = L.icon({
+                        iconUrl: iconPath,
+                        iconSize: [30, 50]
+                    });
+                    const marker = L.marker(new_coords, { icon: customIcon });
+                    marker.bindPopup(`<b>${place.name}</b>`);
+                    markerClusterGroup.addLayer(marker);
+                });
+            }
+
         } catch (error) {
             console.error(error);
         };
@@ -141,16 +175,16 @@ export const LeafletSearch = ({ setSearch }) => {
         // 주소로 검색 -> result에 좌표값, 주소정보 들어있음
         geocoder.addressSearch(search, function (result, status) {
             // 정상적으로 검색이 완료됐으면
-            
+
             if (status === kakao.maps.services.Status.OK) {
                 coords = new L.LatLng(result[0].y, result[0].x);
                 setPosition(coords);
-                
+
                 // user_json 생성 (x좌표,y좌표,반경,편의시설)
                 const user1 = { lon: String(coords.lng), lat: String(coords.lat), radius: radius, facilities_type: facilities.join(',') };
                 const user_json_tmp = JSON.stringify(user1);
                 user_json = JSON.parse(user_json_tmp);
-                
+
                 // address에는 시, 구, 동 단위까지 저장. 결과 컴포넌트에 띄워주기 위함
                 let adrs1, adrs2, adrs3;
                 if (result[0].address) {    // 일반 주소
@@ -183,6 +217,7 @@ export const LeafletSearch = ({ setSearch }) => {
 
                 handleSubmit();
             }
+            // 주소값 잘못 들어왔을 시 alert
             else {
                 alert("올바른 주소를 입력해주세요!")
             }
@@ -315,6 +350,7 @@ export const LeafletSearch = ({ setSearch }) => {
                 <button id="search-btn" onClick={handleSearch}>
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </button>
+                {/* <><div currentLocationSet && setSearch()openSheet() )></div></> */}
                 <div>{showResult && <ResultSheet address={address} coords={position} location={location} />}</div>
             </form>
         </div>
