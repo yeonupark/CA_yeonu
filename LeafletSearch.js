b3fb16110ae4877a2d9c979f306ca48e75080ff1
yeonupark <idepix@naver.com>
2023-06-06 22:40:50 +0900

    편의시설/ 반경 선택 안했을 경우 예외처리

--- LeafletSearch.js
+++ LeafletSearch.js
@@ -169,13 +169,25 @@ export const LeafletSearch = ({ setSearch }) => {
 
     }
     const handleSearch = () => {
+
+        if (facilities.length === 0){
+            return(
+                alert("편의시설을 선택해주세요.")
+                )
+        }
+
+        if (radius === '') {
+            return(
+                alert("반경을 설정해주세요.")
+                )
+        }
+
         // 주소-좌표 변환 객체를 생성
         const geocoder = new kakao.maps.services.Geocoder();
 
         // 주소로 검색 -> result에 좌표값, 주소정보 들어있음
         geocoder.addressSearch(search, function (result, status) {
             // 정상적으로 검색이 완료됐으면
-
             if (status === kakao.maps.services.Status.OK) {
                 coords = new L.LatLng(result[0].y, result[0].x);
                 setPosition(coords);
